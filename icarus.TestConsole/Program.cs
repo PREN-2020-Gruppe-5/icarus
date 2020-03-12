b34187e724feb991ea9af5e4b6328442ff02a19d
Claudio Hübscher <claudio.huebscher@hotmail.com>
2020-03-12 10:35:35 +0100

    Initial solution created

---
+++ icarus.TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace icarus.TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
