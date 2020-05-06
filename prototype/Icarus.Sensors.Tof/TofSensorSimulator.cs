5cf29c681bfe65c8675aa7b188af8b3f958dc2f5
Claudio Hübscher <claudio.huebscher@hotmail.com>
2020-05-06 23:33:39 +0200

    Added TofSensorSimulator

---
+++ prototype/Icarus.Sensors.Tof/TofSensorSimulator.cs
@@ -0,0 +1,17 @@
+﻿namespace Icarus.Sensors.Tof
+{
+    public class TofSensorSimulator : ITofSensor
+    {
+        private double distanceInMillimeters;
+
+        public double GetDistanceInMillimeters()
+        {
+            return this.distanceInMillimeters;
+        }
+
+        public void SetDistanceInMillimeters(double simulatedDistanceInMillimeters)
+        {
+            this.distanceInMillimeters = simulatedDistanceInMillimeters;
+        }
+    }
+}
---
+++ prototype/Icarus.Sensors.Tof/TofSimulationModule.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Icarus.Sensors.Tof
+{
+    public class TofSimulationModule
+    {
+        public static void Initialize(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<ITofSensor, TofSensorSimulator>();
+            serviceCollection.AddSingleton<ITofController, TofController>();
+        }
+    }
+}
