4e372397c8ba1f76608eedcbcefa2e1514965720
Robin Derungs <robin@derungs.app>
2020-05-14 13:11:56 +0200

    Added SpecFlow-Test Project

---
+++ prototype/SpecFlowTest/SpecFlowFeature1Steps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Icarus.Sensors.ObjectDetection;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowTest
+{
+    [Binding]
+    public class SpecFlowFeature1Steps
+    {
+        private readonly IObjectDetectionController objectDetectionController;
+
+        public SpecFlowFeature1Steps()
+        {
+            this.objectDetectionController = new ObjectDetectionController(new ObjectDetectionSensor());
+        }
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int p0)
+        {
+            
+        }
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+
+        }
+
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+
+        }
+    }
+}
